6e0317311dab238e5fea8d5a9fe0c8f98f4a5bd5
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2025-01-12 11:40:52 +0530

    fonts added.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,10 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        Poppins: ["var(--font-Poppins)"],
+        Josefin: ["var(--font-Josefin)"],
+      },
     },
   },
   plugins: [],
