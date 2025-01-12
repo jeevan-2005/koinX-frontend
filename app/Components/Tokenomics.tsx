d28cd16aeceaaae263194526ec5f0ada4dda8799
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2025-01-12 11:36:48 +0530

    Tokenomics info.

---
+++ app/Components/Tokenomics.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import PieChartDistribution from './PieChartDistribution';
+
+type Props = {};
+
+const Tokenomics = (props: Props) => {
+  return (
+    <div className='mt-7 rounded-lg bg-white p-6'>
+      <h3 className='font-Poppins text-[22px] font-semibold'>Tokenomics</h3>
+      <h3 className='mt-6 font-Poppins text-[18px] font-semibold'>
+        Initial Distribution
+      </h3>
+      <div className=''>
+        <PieChartDistribution />
+      </div>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias
+        aspernatur blanditiis consectetur consequuntur culpa doloremque ea eaque
+        et exercitationem facilis id impedit in ipsam ipsum iste iusto,
+        laudantium libero minima molestiae molestias nam nesciunt officia quae
+        quaerat recusandae reiciendis rem repudiandae sed similique tempore
+        temporibus totam veniam voluptatem. Odio, porro?Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi quisquam iusto magnam ab ut molestiae accusantium neque animi, deserunt laboriosam? Totam ea inventore eius accusamus sed similique iste aut nesciunt. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, iure! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis, nobis.
+      </p>
+    </div>
+  );
+};
+
+export default Tokenomics;
